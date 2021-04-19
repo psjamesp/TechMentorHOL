aa6da3217cf9256903147bd281c7031fb5011426
James Petty <james.petty714@outlook.com>
2021-04-19 14:48:32 -0400

    back to green

--- Lession1 - Fun with Profiles.ps1
+++ Lession1 - Fun with Profiles.ps1
@@ -7,4 +7,4 @@ code .\profile.ps1
 Install-Module posh-git
 
 $host.PrivateData.ErrorBackgroundColor = "magenta"
-$host.PrivateData.ErrorForegroundColor = "yellow" 
+$host.PrivateData.ErrorForegroundColor = "green" 
